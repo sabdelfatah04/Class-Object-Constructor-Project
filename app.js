56390dbbdcc08c1bf5590e5d74250f39ac689584
sabdelfatah04 <50589044+sabdelfatah04@users.noreply.github.com>
2019-10-10 11:50:39 -0400

    sdgf

--- app.js
+++ app.js
@@ -2,36 +2,24 @@
 ///class to create the object (person, album, movie)***Model
 //take arguments
 class Element { 
-    constructor(name, atomicNumber, massNumber){
+    constructor(name, atomicNumber, atomicMass){
         this.name = name;
         this.atomicNumber = atomicNumber;
-        this.massNumber = massNumber;
+        this.atomicMass = atomicMass;
     }
-    calculateProtons(){
+    /* calculateProtons(){
         return this.massNumber - this.atomicNumber;
-    }
+    } */
 }
-
-
-
 ///**View class fro the UI which contains methods
 //clear field methods
 
 //remove something/object (remove avenger)
 
 //creates/insert the html for the UI display
-
-const form = document.querySelector('#hero-form');// could also getElementById
-const userAlias = document.querySelector('#alias');
-const userPowers = document.querySelector('#powers');
-const userFullName = document.querySelector('#full-name');
-const userFirstAppearance = document.querySelector('#first-appearance');
-const userImage = document.querySelector('#image');
-const display = document.querySelector('.display');
-
 function eventListeners(){
-    form.addEventListener('submit', displayAvengerMember);
-    display.addEventListener('click', removeAvenger);
+    form.addEventListener('submit', displayPeriodicElement);
+    display.addEventListener('click', removeElement);
     form.addEventListener('submit', function(){
         console.log('test');
     });
@@ -39,16 +27,19 @@ function eventListeners(){
 eventListeners();
 
 function displayPeriodicElement(e){
-    if (userAlias.value||userPowers.value||userFullName.value||userFirstAppearance.value||userImage.value === '') {
+    const form = document.querySelector('#element-form');
+    const userName = document.querySelector('#name');
+    const userAtomicNumber = document.querySelector('#atomicNumber');
+    const userAtomicMass = document.querySelector('#atomicMass');
+    const display = document.querySelector('.display');
+    if (userName.value||useratomicNumber.value||useratomicMass.value === '') {
         alert('Error: Missing Info')
       } else {
-        let html = ' <div class="display-element"><div class="display-name">%name%</div><div class="display-atomicNumber">%atomicNumber%</div><div class="display-atomicMass">%atomicNumber%</div><div class="remove-element"><p class="remove-element">Remove Element &#10006; </p></div></div>';
+        let html = ' <div class="display-element"><div class="display-name">%name%</div><div class="display-atomicNumber">%atomicNumber%</div><div class="display-atomicMass">%atomicMass%</div><div class="remove-element"><p class="remove-element">Remove Element &#10006; </p></div></div>';
 
-    let newHtml = html.replace('%alias%', userAlias.value);
-    newHtml = newHtml.replace('%powers%', userPowers.value);
-    newHtml = newHtml.replace('%name%', userFullName.value);
-    newHtml = newHtml.replace('%years%', userFirstAppearance.value);
-    newHtml = newHtml.replace('%url%', userImage.value);
+    let newHtml = html.replace('%name%', userName.value);
+    newHtml = newHtml.replace('%atomicNumber%', userAtomicNumber.value);
+    newHtml = newHtml.replace('%atomicMass%', userAtomicMass.value);
     display.insertAdjacentHTML('beforeend', newHtml);
     init();
     e.preventDefault();
@@ -57,18 +48,16 @@ function displayPeriodicElement(e){
       
 }
 
-function removeAvenger(e) {
-    if(e.target.parentElement.classList.contains('remove-avenger')){
+function removeElement(e) {
+    if(e.target.parentElement.classList.contains('remove-element')){
         e.target.parentElement.parentElement.remove();
         console.log(e.target.parentElement);
     }
 }
 function init(){
-    userAlias.value = '';
-    userPowers.value = '';
-    userFullName.value = '';
-    userFirstAppearance.value = '';
-    userImage.value = '';
+    userName.value = '';
+    userAtomicMass.value = '';
+    userAtomicNumber.value = '';
 }
 init();
 
