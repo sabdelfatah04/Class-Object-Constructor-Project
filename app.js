a96c3c21688cb7bfbe60a1fd1960f697e322fa71
sabdelfatah04 <50589044+sabdelfatah04@users.noreply.github.com>
2019-10-18 16:15:18 -0400

    done

--- app.js
+++ app.js
@@ -1,74 +1,67 @@
 class Element {
-  constructor(name, atomicNumber, atomicMass) {
+  constructor(name, atomicNumber, yearDiscovered) {
     this.name = name;
     this.atomicNumber = atomicNumber;
-    this.atomicMass = atomicMass;
-  }
-  /* calculateProtons(){
-        return this.massNumber - this.atomicNumber;
-    } */
+    this.yearDiscovered = yearDiscovered;
+    this.discovered = function(){
+       return new Date().getFullYear() - this.yearDiscovered;
+   }}
 }
 class UI {
-  addPeriodicElement(_element) {
-    if (
-      name ||
-      atomicNumber ||
-      atomicMass === ""
-    ) {
-      alert("Error: Missing Info");
-    } else {
-      let html =
-        ' <div class="display-element"><div class="display-name">%name%</div><div class="display-atomicNumber">%atomicNumber%</div><div class="display-atomicMass">%atomicMass%</div><div class="remove-element"><p class="remove-element">Remove Element &#10006; </p></div></div>';
+  addPeriodicElement(element) {
 
-      let newHtml = html.replace("%name%", userName.value);
-      newHtml = newHtml.replace("%atomicNumber%", userAtomicNumber.value);
-      newHtml = newHtml.replace("%atomicMass%", userAtomicMass.value);
+      let html =
+        ' <div class="display-element"><div class="display-name">%name%</div><div class="display-atomicNumber">%atomicNumber%</div><div class="display-yearDiscovered">%yearDiscovered%</div><div class="display-discovered">%discovered%</div><div class = "remove-element">X Remove Element</div>';
+      const currentYear = new Date().getFullYear();
+      const discovered = currentYear - yearDiscovered.value;
+      let newHtml = html.replace('%name%', element.name);
+      newHtml = newHtml.replace('%atomicNumber%', element.atomicNumber);
+      newHtml = newHtml.replace('%yearDiscovered%', element.yearDiscovered);
+      newHtml = newHtml.replace('%discovered%', discovered);
       document
         .querySelector(".display")
-        .insertAdjacentHTML("beforeend", newHtml);
+        .insertAdjacentHTML('beforeend', newHtml);
+        console.log(discovered);
     }
-  }
   
-  removeElement(e) {
-    if (EventTarget.className ==="remove-element"){
-      EventTarget.parentElement.remove();
-      }
-    }
-
-
   reset() {
     document.getElementById('name').value = "";
-    document.getElementById("atomicNumber").value;
-    document.getElementById("atomicMass").value;
+    document.getElementById('atomicNumber').value = "";
+    document.getElementById('yearDiscovered').value = "";
+  }
+
+  removeElement(target) {
+    if (target.className === "remove-element"){
+      target.parentElement.remove();
+      }
   }
 
 }
 
-function eventListeners() {
-  form.addEventListener("submit", ui.addPeriodicElement);
-  display.addEventListener("click", removeElement);
-}
- 
-document.getElementById("form").addEventListener("submit", function(e) {
+document.getElementById('form').addEventListener('submit', function(e) {
+
+  
   const name = document.getElementById("name").value;
   const atomicNumber = document.getElementById("atomicNumber").value;
-  const atomicMass = document.getElementById("atomicMass").value;
+  const yearDiscovered = document.getElementById("yearDiscovered").value;
 
-  const element = new Element(name, atomicNumber, atomicMass);
+  const element = new Element(name, atomicNumber, yearDiscovered);
   //console.log(element);
 
   const ui = new UI();
   console.log(ui);
-
+ if (name ==='' || atomicNumber ==='' || yearDiscovered ===''){
+    alert ('Error:Missing Info')
+  } else {
   ui.addPeriodicElement(element);
-
   ui.reset();
-
-  e.preventDefault();
+   }
+e.preventDefault();
 });
+
 document.querySelector(".display").addEventListener("click", function(e){
   const ui = new UI();
-  ui.addPeriodicElement(element);
+  ui.removeElement(e.target);
   ui.reset();
   e.preventDefault();
 }
